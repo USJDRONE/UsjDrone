7dd34ce2ffd21ea8fc6c8319d874db3757d3a3dc
Moudiz <mohamadkerdly@gmail.com>
2019-06-12 11:23:48 +0300

    First commit.

---
+++ TelloDrone/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace TelloDrone
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.droneStream = new WebEye.Controls.WinForms.StreamPlayerControl.StreamPlayerControl();
+            this.bttnCon = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // droneStream
+            // 
+            this.droneStream.AutoSize = true;
+            this.droneStream.BackColor = System.Drawing.Color.Black;
+            this.droneStream.Cursor = System.Windows.Forms.Cursors.Default;
+            this.droneStream.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.droneStream.Location = new System.Drawing.Point(0, 0);
+            this.droneStream.Name = "droneStream";
+            this.droneStream.Size = new System.Drawing.Size(1267, 678);
+            this.droneStream.TabIndex = 0;
+            // 
+            // bttnCon
+            // 
+            this.bttnCon.BackColor = System.Drawing.SystemColors.Control;
+            this.bttnCon.Dock = System.Windows.Forms.DockStyle.Left;
+            this.bttnCon.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bttnCon.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttnCon.Location = new System.Drawing.Point(0, 0);
+            this.bttnCon.Name = "bttnCon";
+            this.bttnCon.Size = new System.Drawing.Size(168, 60);
+            this.bttnCon.TabIndex = 1;
+            this.bttnCon.Text = "Disconnect";
+            this.bttnCon.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.bttnCon);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 618);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1267, 60);
+            this.panel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(581, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1267, 678);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.droneStream);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private WebEye.Controls.WinForms.StreamPlayerControl.StreamPlayerControl droneStream;
+        private System.Windows.Forms.Button bttnCon;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TelloDrone/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TelloLib;
+
+namespace TelloDrone
+{
+    using WebEye.Controls.WinForms.StreamPlayerControl;
+
+    public partial class Form1 : Form
+    {
+        static float sensitivity = 0.5f;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            //droneStream.StartPlay(new Uri("rtsp://184.72.239.149/vod/mp4:BigBuckBunny_115k.mov"));
+
+            Tello.onConnection += (Tello.ConnectionState newState) =>
+            {
+                if (newState != Tello.ConnectionState.Connected)
+                {
+                }
+                if (newState == Tello.ConnectionState.Connected)
+                {
+                    Tello.setMaxHeight(5);
+                }
+                label1.Text = ("Tello " + newState.ToString());
+            };
+            Tello.startConnecting();
+
+            var videoClient = UdpUser.ConnectTo("127.0.0.1", 7038);
+
+            Tello.onVideoData += (byte[] data) =>
+            {
+                try
+                {
+                    videoClient.Send(data.Skip(2).ToArray());//Skip 2 byte header and send to ffplay. 
+                    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error in Video Feed: " + ex.ToString());
+                }
+            };
+
+            droneStream.StartPlay(new Uri("udp://127.0.0.1:7038"));
+            //var videoClient = UdpUser.ConnectTo("192.168.10.1", 11111);
+            //droneStream.StartPlay(new Uri("udp://192.168.10.1:11111"));
+        }
+
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            float[] axis = new float[] { 0.0f, 0.0f, 0.0f, 0.0f, 0.0f };
+            switch (keyData)
+            {
+
+                case Keys.W:                   
+                    axis[3] = sensitivity;         
+                    break;
+
+                case Keys.A:                   
+                    axis[2] = -sensitivity;         
+                    break;
+
+                case Keys.S:
+                    axis[3] = -sensitivity;
+                    break;
+
+                case Keys.D:
+                    axis[2] = sensitivity;
+                    break;
+
+                case Keys.Q:                    
+                    axis[0] = sensitivity;                    
+                    break;
+
+                case Keys.E:                    
+                    axis[0] = -sensitivity;
+                    break;
+
+                case Keys.Z:                    
+                    axis[1] = sensitivity;                    
+                    break;
+
+                case Keys.X:                    
+                    axis[1] = -sensitivity;                    
+                    break;
+
+                case Keys.D1:                    
+                    if (sensitivity + 0.25f <= 1f)
+                        sensitivity += 0.25f;
+                    break;
+
+                case Keys.D2:
+                    if (sensitivity - 0.25f >= 0.0f)
+                        sensitivity -= 0.25f;
+                    break;
+
+                case Keys.Enter:
+                    takepic();
+                    break;
+
+                case Keys.P:
+                    sensitivity = 0.5f;
+                    break;
+
+                case Keys.Tab:
+                    {
+                        if (Tello.connected)
+                        {
+                            if (Tello.state.flying)
+                            {
+                                Tello.land();
+                            }
+                            else
+                            {
+                                Tello.takeOff();
+                            }
+                        }
+                    }
+                    break;
+            }
+            Tello.controllerState.setAxis(axis[0], axis[1], axis[2], axis[3]);
+
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+        
+        private void takepic()
+        {
+            Bitmap img = droneStream.GetCurrentFrame();
+            img.Save(@"C:\Users\Mohamad K\Desktop\pic.png", ImageFormat.Png);
+        }
+
+
+        private void BttnPic_Click(object sender, EventArgs e)
+        {
+            takepic();
+        }
+    }
+}
